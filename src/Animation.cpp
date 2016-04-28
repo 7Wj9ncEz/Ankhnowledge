dd199c5457af6f8db46055ed716cbf2184d78368
Niets <nicolas.boarin@gmail.com>
2016-04-28 00:25:45 -0300

    Assertives + camelCase to snake_case

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -6,19 +6,26 @@
  */
 
 #include "Animation.h"
+#include <assert.h>
 
-Animation::Animation(int spriteWidth, int spriteHeight, Sprite* sprite, int direction) {
-    //instancia um novo sprite
+Animation::Animation(int sprite_width, int sprite_height, Sprite* sprite, int direction) {
+
+    // Asserts
+    assert(sprite != NULL);
+    assert(sprite_width >= 0);
+    assert(sprite_height >= 0);
+
+    // Makes a new sprite instance
     this->animation = sprite;
-    this->spriteWidth = spriteWidth;
-    this->spriteHeight = spriteHeight;
-    this->columns =  animation->getWidth() / spriteWidth;
+    this->sprite_width = sprite_width;
+    this->sprite_height = sprite_height;
+    this->columns =  animation->getWidth() / sprite_width;
     this->dt = 0;
-    this->startFrame = direction * columns;
-    this->prevDirection = direction;
-    this->currentFrame = startFrame;
-    this->animateSprite = true;
-    this->finishedAnimation = false;
+    this->start_frame = direction * columns;
+    this->previous_direction = direction;
+    this->current_frame = start_frame;
+    this->animate_sprite = true;
+    this->finished_animation = false;
 
 }
 
@@ -26,59 +33,72 @@ Animation::~Animation() {
 	// TODO Auto-generated destructor stub
 }
 
-void Animation::render(int index, float posX, float posY) {
+void Animation::render(int index, float pos_x, float pos_y) {
+
+  // Asserts
+  assert(index >= 0);
+  assert(pos_x >= 0);
+  assert(pos_y >= 0);
 
 	int x = index % columns;
 	int y = index / columns;
 
-	int clipx = x * spriteWidth;
-	int clipy = y * spriteHeight;
+	int clip_x = x * sprite_width;
+	int clip_y = y * sprite_height;
 
-	animation->clip(clipx, clipy, spriteWidth, spriteHeight);
-	animation->render(posX, posY);
+	animation->clip(clip_x, clip_y, sprite_width, sprite_height);
+	animation->render(pos_x, pos_y);
 }
 
-void Animation::animate(int frameRate,float posX, float posY) {
-	this->frameRate = frameRate;
-	render(currentFrame,posX,posY);
+void Animation::animate(int frame_rate, float pos_x, float pos_y) {
+
+  // Asserts
+  assert(frame_rate >= 0);
+
+  this->frame_rate = frame_rate;
+	render(current_frame, pos_x, pos_y);
 }
 
-void Animation::update(int dt, bool onLoop, int direction, bool singleFrame) {
+void Animation::update(int dt, bool on_loop, int direction, bool single_frame) {
+
+  // Asserts
+  assert(dt >= 0);
+
 	this->dt = this->dt + dt;
-	int lastFrame = columns;
+	int last_frame = columns;
 
-	if (prevDirection != direction) {
-		this->startFrame = direction * columns;
-		this->currentFrame = startFrame;
-		this->prevDirection = direction;
-		this->finishedAnimation = false;
+	if (previous_direction != direction) {
+		this->start_frame = direction * columns;
+		this->current_frame = start_frame;
+		this->previous_direction = direction;
+		this->finished_animation = false;
 	}
 
   else {
     // Nothing to do
   }
 
-	if(singleFrame) {
-		lastFrame = 1;
-		this->finishedAnimation = false;
+	if (single_frame) {
+		last_frame = 1;
+		this->finished_animation = false;
 	}
 
   else {
     // Nothing to do
   }
 
-	if((this->dt > frameRate)) {
-		if ((currentFrame == startFrame + lastFrame -1) && (onLoop == true)) {
-			resetStartFrame();
-			finishedAnimation = false;
+	if ((this->dt > frame_rate)) {
+		if ((current_frame == start_frame + last_frame -1) && (on_loop == true)) {
+			reset_start_frame();
+			finished_animation = false;
 		}
 
-    else if ((currentFrame == startFrame + lastFrame -1) && (onLoop == false)) {
-			this->finishedAnimation = true;
+    else if ((current_frame == start_frame + last_frame -1) && (on_loop == false)) {
+			this->finished_animation = true;
 		}
 
 		else {
-			currentFrame++;
+			current_frame++;
 			this->dt = 0;
 		}
 	}
@@ -89,10 +109,10 @@ void Animation::update(int dt, bool onLoop, int direction, bool singleFrame) {
 
 }
 
-void Animation::resetStartFrame() {
-	this->currentFrame = startFrame;
+void Animation::reset_start_frame() {
+	this->current_frame = start_frame;
 }
 
-bool Animation::getFinishedAnimation() {
-	return this->finishedAnimation;
+bool Animation::get_finished_animation() {
+	return this->finished_animation;
 }
